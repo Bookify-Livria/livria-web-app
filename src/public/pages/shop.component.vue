efc6af498ae61e64ab7516d480f2b9453537e82a
Melisa Geraldine Sulca Silva <u202224602@upc.edu.pe>
2025-05-15 07:50:07 -0500

    Update shop.component.vue

--- src/public/pages/shop.component.vue
+++ src/public/pages/shop.component.vue
@@ -20,7 +20,7 @@ export default defineComponent({
       <img src="../../assets/images/store/shopLivria.png" alt="Store_img" class="shop-photo" />
     </div>
     <div class="shop-right">
-      <h4 class="h3__title">{{$t("address")}}</h4>
+      <h3 class="h3__title">{{$t("address")}}</h3>
       <p>Av. República de Chile 661, Jesús María 15072, Peru</p>
       <iframe
           class="shop-map"
@@ -37,3 +37,81 @@ export default defineComponent({
     <footerComponent/>
   </div>
 </template>
+
+<style scoped>
+.shop-container {
+  display: flex;
+  align-items: flex-start;
+  gap: 2rem;
+  padding: 17rem 3rem 9.5rem;
+  flex-wrap: wrap;
+}
+
+.shop-left, .shop-right {
+  flex: 1 1 350px;
+  min-width: 280px;
+  max-width: 600px;
+  box-sizing: border-box;
+}
+
+.shop-left {
+  text-align: center;
+  background-color: #213547;
+  border-radius: 8px;
+  color: #eefafa;
+  padding: 1rem 1.5rem 2.5rem;
+  margin-bottom: 2rem;
+  width: 100%;
+}
+
+.shop-photo {
+  width: 100%;
+  max-width: 380px;
+  height: auto;
+  border-radius: 8px;
+}
+
+.shop-right {
+  text-align: center;
+  border-radius: 8px;
+  padding: 2rem 1.5rem 0;
+  margin-bottom: 2rem;
+  color: #feb913;
+  font-size: 1rem;
+}
+
+.shop-right p{
+  text-align: center;
+}
+
+.shop-map {
+  margin-top: 1rem;
+  width: 100%;
+  max-width: 450px;
+  height: 205px;
+  border-radius: 8px;
+}
+
+/* Media queries para pantallas pequeñas */
+@media (max-width: 900px) {
+  .shop-container {
+    flex-direction: column;
+    align-items: stretch;
+    gap: 1.5rem;
+    padding: 18rem 0.5rem 2rem;
+  }
+  .shop-left, .shop-right {
+    max-width: 100%;
+    min-width: 0;
+    padding: 1rem 4rem 3rem;
+  }
+  .shop-photo, .shop-map {
+    max-width: 100%;
+  }
+}
+
+.footer__container-fullwidth {
+  width: 100vw;
+  margin-left: calc(-1 * ((100vw - 100%) / 2));
+}
+</style>
